ea3b2abe2ad3596ccfe59fb80b2971df121bbb0e
Adam Cigánek <adam.ciganek@gmail.com>
2016-04-29 15:26:26 +0200

    Version change to 0.6.135-test

---
+++ ci/travis/deploy_docker_image.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+set -ev
+
+export RUST_BACKTRACE=1
+cargo build --target $TARGET --release
+
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+
+BUILD_DIR=installer/docker
+
+cp target/$TARGET/release/$PROJECT_NAME $BUILD_DIR
+cp -r installer/bundle/* $BUILD_DIR
+
+pushd $BUILD_DIR
+docker build -t $DOCKER_IMAGE:$PROJECT_VERSION .
+docker tag $DOCKER_IMAGE:$PROJECT_VERSION $DOCKER_IMAGE:latest
+popd
+
+docker push $DOCKER_IMAGE:$PROJECT_VERSION
